63ec58a4f8da0c7d1a99573691ca940c67ce34aa
dan041223 <danielgarcia.barquilla@hotmail.com>
2024-06-23 19:15:10 +0200

    Animacion del diamante

--- Plataformas2D/scripts/items/big_diamond.gd
+++ Plataformas2D/scripts/items/big_diamond.gd
@@ -2,10 +2,12 @@ extends Area2D
 
 class_name Diamond
 	
+@onready var obtainSound : AudioStreamPlayer = $DiamondObtainSound;
+@onready var animation : AnimationPlayer = $AnimationPlayer;
 
 func _on_body_entered(body):
 	if body is Player:
-		$DiamondObtainSound.play();
-		hide();
+		animation.play("Obtain");
+		obtainSound.play();
 		await get_tree().create_timer(0.20).timeout;
 		queue_free();
